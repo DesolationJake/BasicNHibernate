8d7d211eaf3a8d9643f958a7a3b8bbe01177e431
Jake Helme <desolationjake@gmail.com>
2016-06-01 19:09:34 +0200

    Tutorial from NHibernate docs, updated to new versions of NUnit and NHibernate

---
+++ NHibernateFirst.Tests/GenerateSchemaFixture.cs
@@ -0,0 +1,21 @@
+﻿using BasicNHibernate.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace BasicNHibernate.Tests
+{
+    [TestFixture]
+    public class GenerateSchemaFixture
+    {
+        [Test]
+        public void Can_generate_schema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Product).Assembly);
+
+            new SchemaExport(cfg).Execute(true, true, false);
+        }
+    }
+}
---
+++ NHibernateFirst/Domain/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicNHibernate.Domain
+{
+    public class Product
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Category { get; set; }
+        public virtual bool Discontinued { get; set; }
+    }
+}
